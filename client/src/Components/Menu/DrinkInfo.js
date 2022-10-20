977d1c88b68490a97b7d04dd8c0fc5ad5a6082c3
Marcus Otterstad <inet2@otterstad.name>
2022-10-20 12:14:19 +0200

    styling and html on DrinkInfo

--- client/src/Components/Menu/DrinkInfo.js
+++ client/src/Components/Menu/DrinkInfo.js
@@ -1,13 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom';
+import img_1 from './img_1.jpg';
+
 
 //Component to render single drink in a page
 function DrinkInfo() {
 
-
     const params = useParams();
-    const [data, setData] = useState([]);
-    const {name, description, current_price, price_history} = data;
+    const [data, setData] = useState({});
+    const {name, description, currentPrice, priceHistory} = data;
 
     useEffect(() => {
         const fetchData = async () => {
@@ -19,11 +20,23 @@ function DrinkInfo() {
         ;
     }, []);
 
+    return (
+    <div className='row'>
+        <div className='col-sm text-center row'>
+            <h2 className='m-4'>{name}</h2>
 
-    return (<div>
-        <p>{name}</p>
-        <p>{description}</p>
-        <p>{current_price}</p>
+            <div className='col-sm'>
+                <p>current price: </p><p className='text-success'>{currentPrice} kr</p>
+                <p>{description}</p>
+            </div>
+
+            <div className='col-sm'>
+                <img className='img-fluid' src={img_1} />
+            </div>
+
+        </div>
+        <div className='col-sm '>
+        </div>
     </div>);
 }
 
--- server/src/routes/menuRouter.js
+++ server/src/routes/menuRouter.js
@@ -8,18 +8,18 @@ menuRouter.get("/", getQuery('SELECT * FROM "drinks"'));
 
 menuRouter.get("/:drink_id", (req, res) => {
     const drink_id = req.params.drink_id;
-
     
     Promise.all([
         pool.query('SELECT name, description, current_price FROM drinks WHERE drink_id = $1', [drink_id]),
-        pool.query('SELECT timestamp, price FROM price_history WHERE drink_id = $1', [drink_id])
+        pool.query('SELECT DATE(timestamp) as timestamp, price FROM price_history WHERE drink_id = $1', [drink_id])
         ]).then(function([drink_info, price_history]) {
             const {name, description, current_price} = drink_info.rows[0]
+            const formattedPriceHistory = price_history.rows.map(obj => [obj.price, obj.timestamp]);
             const formattedObject = {
                 name: name,
                 description: description,
                 currentPrice: current_price,
-                priceHistory: price_history.rows
+                priceHistory: formattedPriceHistory
             }
             res.status(200).json(formattedObject)
         }, function(error) {

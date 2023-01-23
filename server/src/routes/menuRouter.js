8fa8b0c39e6a021e59ed0f9df069e0cadf9b211a
Marcus Otterstad <inet2@otterstad.name>
2023-01-23 12:34:01 +0100

    finished most tests on menu/:drink_id and menu/:drink_id/price, all pass

--- server/src/routes/menuRouter.js
+++ server/src/routes/menuRouter.js
@@ -12,29 +12,37 @@ menuRouter.get("/", getQuery('SELECT * FROM "drinks"'));
 // GET single drink information
 menuRouter.get("/:drink_id", (req, res) => {
     const drink_id = req.params.drink_id;
+
+        if(isNaN(drink_id)) {
+            res.status(400).json({message: "Bad request. Make sure to input a number"})
+            return
+        }
+
+
     
     Promise.all([
         pool.query('SELECT name, description, current_price FROM drinks WHERE drink_id = $1', [drink_id]),
         pool.query('SELECT DATE(timestamp) as timestamp, price FROM price_history WHERE drink_id = $1', [drink_id])
         ]).then(function([drink_info, price_history]) {
 
-
             //validate if the database returned a row
 
-            if(drink_info.rows.length != 0 && price_history.rows.length  != 0) {
-                const {name, description, current_price} = drink_info.rows[0]
-                const formattedPriceHistory = price_history.rows.map(obj => [obj.price, obj.timestamp]);
-                const formattedObject = {
-                    name: name,
-                    description: description,
-                    currentPrice: current_price,
-                    priceHistory: formattedPriceHistory
-                }
-                res.status(200).json(formattedObject)
-            }
-            else {
+            if(drink_info.rows.length == 0 || price_history.rows.length  == 0) {
                 res.status(404).json({message: "drink does not exist in database"})
+                return
             }
+
+            const {name, description, current_price} = drink_info.rows[0]
+            const formattedPriceHistory = price_history.rows.map(obj => [obj.price, obj.timestamp]);
+            const formattedObject = {
+                name: name,
+                description: description,
+                currentPrice: current_price,
+                priceHistory: formattedPriceHistory
+            }
+            res.status(200).json(formattedObject)
+            
+
         }, function(error) {
         throw error;
         });
@@ -43,7 +51,13 @@ menuRouter.get("/:drink_id", (req, res) => {
 menuRouter.get("/:drink_id/price", async (req, res) => {
     const drink_id = req.params.drink_id;
 
+    if(typeof Number(drink_id) !== "number") {
+        res.status(400).json({message: "Bad request. Make sure to input a number"})
+        return
+    }
+
     pool.query('SELECT current_price FROM drinks WHERE drink_id = $1', [drink_id], (error, results) => {
+
         if (error) {
             res.status(404).json({message: "Failed to retrieve from database."});
             return
--- server/test/unit/routes/menuRouter.test.js
+++ server/test/unit/routes/menuRouter.test.js
@@ -3,64 +3,76 @@ const chai = require('chai');  // Using Expect style
 const pool = require('../../../src/configs/db.config')
 
 const app = require('../../../index');
+const { expect } = require('chai');
 
-// GET full list over all items in the menu 
-describe('GET /menu', () => { 
-    test("should return 200", async () => {
-        const response = await request(app).get("/menu");
-        
-        expect(response.statusCode).toBe(200);
+describe('/menu router', () => { 
+
+    // GET full menu
+    describe('GET /menu', () => { 
+        test("should return 200", async () => {
+            const response = await request(app).get("/menu");
+            
+            expect(response.statusCode).equal(200);
+        });
+
+        test("should have more than 0 rows", async () => {
+            const response = await request(app).get("/menu");
+            
+            expect(response.body.length).not.equal(0);
+        }); 
     });
 
-    test("should have more than 0 rows", async () => {
-        const response = await request(app).get("/menu");
-        
-        expect(response.body.length).not.toBe(0);
-    }); 
-});
+
+    // GET info of a single dirnk
+    describe('GET /menu/:drink-id/', () => {
+        test("valid query should return 200", async () => {
+            const response = await request(app).get("/menu/1/");
+
+            expect(response.statusCode).equal(200);
+        });
+
+        test("valid query should have more than 0 rows", async () => {
+            const response = await request(app).get("/menu/1/");
+            
+            expect(response.body.length).not.equal(0);
+        }); 
+
+        test("drink that does not exist in database should return status code 404", async () => {
+            const response = await request(app).get("/menu/3000/");
+            
+            expect(response.statusCode).equal(404);
+        });
+
+        test("non-integer should return status code 400", async () => {
+            const response = await request(app).get("/menu/a/");
+            
+            expect(response.statusCode).equal(400);
+        });
+
+    })
 
 
-// GET info of a single dirnk
-describe('GET /menu/:drink-id/', () => {
-    test("valid query should return 200", async () => {
-        const response = await request(app).get("/menu/1/");
+    // GET price of single drink
+    describe('GET /menu/:drink_id/price/', () => {
 
-        expect(response.statusCode).toBe(200);
+        test("valid query should return 200", async () => {
+            const response = await request(app).get("/menu/1/price");        
+            
+            expect(response.statusCode).equal(200);
+        });
+
+        test("drink that is not in menu should return 404", async () => {
+            const response = await request(app).get("/menu/3000/price");        
+
+            expect(response.statusCode).equal(404);
+        });
+
+        test("price should not be 0", async () => {
+            const response = await request(app).get("/menu/1/price");        
+
+            expect(response.body.price).not.equal(0);
+        });
     });
 
-    test("valid query should have more than 0 rows", async () => {
-        const response = await request(app).get("/menu/1/");
-        
-        expect(response.body.length).not.toBe(0);
-    }); 
-
-    test("drink that does not exist in database should return 404", async () => {
-        const response = await request(app).get("/menu/3000/");
-        
-        expect(response.statusCode).toBe(404);
-    });
-})
-
-
-// GET price of single drink
-describe('GET /menu/:drink_id/price/', () => {
-
-    test("valid query should return 200", async () => {
-        const response = await request(app).get("/menu/1/price");        
-        
-        expect(response.statusCode).toBe(200);
-    });
-
-    test("drink that is not in menu should return 404", async () => {
-        const response = await request(app).get("/menu/3000/price");        
-        
-        expect(response.statusCode).toBe(404);
-    });
-
-    test("price should not be 0", async () => {
-        const response = await request(app).get("/menu/1/price");        
-
-        expect(response.body.price).not.toBe(0);
-    });
 
 });
